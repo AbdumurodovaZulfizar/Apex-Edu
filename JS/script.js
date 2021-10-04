e668287d57ebeaeabad014f1ee7d76cdc7d9215f
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-10-04 19:16:24 +0500

    Add active property to slide controller

--- JS/script.js
+++ JS/script.js
@@ -8,8 +8,50 @@ menu_container.addEventListener('click', () => {
 
 window.addEventListener('DOMContentLoaded', () => {
   const slides = document.querySelectorAll('.offer_slide');
-  const wrapper = document.querySelector('.carusel-container');
   const slidesFile = document.querySelector('.carusel-inner');
-  const width = getComputedStyle(wrapper);
   slidesFile.style.width = slides.length * 100 + '%';
+})
+
+const setActiveButton = (button) => {
+  const buttons = document.querySelectorAll('.lines');
+
+  buttons.forEach((button) => {
+    if (button !== this) {
+      button.classList.remove('active');
+    }
+  });
+  button.classList.add('active');
+};
+
+const slider = document.querySelector('.carusel-inner');
+const active = document.getElementById('active');
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('line1')) {
+    if (e.target.classList.contains('active')) return;
+    setActiveButton(e.target);
+    slider.style.transform = 'translateX(0)';
+  } else if (e.target.classList.contains('line2')) {
+    if (e.target.classList.contains('active')) return;
+    setActiveButton(e.target);
+    slider.style.transform = 'translateX(-33.33%)';
+  } else if (e.target.classList.contains('line3')) {
+    if (e.target.classList.contains('active')) return;
+    setActiveButton(e.target);
+    slider.style.transform = 'translateX(-66.66%)';
+  }
+})
+
+line1.addEventListener('click', () => {
+  
+})
+
+line2.addEventListener('click', () => {
+  
+})
+
+line3.addEventListener('click', () => {
+  
 })
