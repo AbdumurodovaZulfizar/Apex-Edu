8fb8e4ed29f75d0e55dacac2b71b108a3bab8ec3
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-10-04 17:22:15 +0500

    Create slider

--- JS/script.js
+++ JS/script.js
@@ -4,4 +4,12 @@ const menu_container = document.getElementById('menu-container');
 
 menu_container.addEventListener('click', () => {
   navbar.classList.toggle('d-none');
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const slides = document.querySelectorAll('.offer_slide');
+  const wrapper = document.querySelector('.carusel-container');
+  const slidesFile = document.querySelector('.carusel-inner');
+  const width = getComputedStyle(wrapper);
+  slidesFile.style.width = slides.length * 100 + '%';
 })
