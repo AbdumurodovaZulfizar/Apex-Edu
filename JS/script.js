f4da092e5ad0b9477757af50ef8f187261dd691f
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-10-05 11:08:46 +0500

    Fix linter errors

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,18 @@
 const menu = document.getElementById('menu-container');
 const navbar = document.getElementById('navbar');
 
-menu.addEventListener('click', (e) => {
+const setActiveButton = (button, str) => {
+  const buttons = document.querySelectorAll(str);
+
+  buttons.forEach((button) => {
+    if (button !== this) {
+      button.classList.remove('active');
+    }
+  });
+  button.classList.add('active');
+};
+
+menu.addEventListener('click', () => {
   if (menu.classList.contains('close')) {
     menu.innerHTML = `<div class="open-bar-1"></div>
     <div class="open-bar-2"></div>`;
@@ -16,20 +27,21 @@ menu.addEventListener('click', (e) => {
     menu.classList.toggle('open');
     navbar.classList.toggle('d-none');
   }
-})
+});
+
 window.addEventListener('DOMContentLoaded', () => {
   const slides = document.querySelectorAll('.offer_slide');
   const slidesFile = document.querySelector('.carusel-inner');
-  const slides_2 = document.querySelectorAll('.offer_slide-kurslar');
-  const slidesFile_2 = document.querySelector('.carusel-inner-kurslar');
-  const slides_3 = document.querySelectorAll('.offer_slide-ustozlar');
-  const slidesFile_3 = document.querySelector('.carusel-inner-ustozlar');
-  const slides_4 = document.querySelectorAll('.offer_slide-videolar');
-  const slidesFile_4 = document.querySelector('.carusel-inner-videolar');
-  const slides_5 = document.querySelectorAll('.offer_slide-oquvchilar');
-  const slidesFile_5 = document.querySelector('.carusel-inner-oquvchilar');
-  const slides_6 = document.querySelectorAll('.offer_slide-mijozlar');
-  const slidesFile_6 = document.querySelector('.carusel-inner-mijozlar');
+  const slides2 = document.querySelectorAll('.offer_slide-kurslar');
+  const slidesFile2 = document.querySelector('.carusel-inner-kurslar');
+  const slides3 = document.querySelectorAll('.offer_slide-ustozlar');
+  const slidesFile3 = document.querySelector('.carusel-inner-ustozlar');
+  const slides4 = document.querySelectorAll('.offer_slide-videolar');
+  const slidesFile4 = document.querySelector('.carusel-inner-videolar');
+  const slides5 = document.querySelectorAll('.offer_slide-oquvchilar');
+  const slidesFile5 = document.querySelector('.carusel-inner-oquvchilar');
+  const slides6 = document.querySelectorAll('.offer_slide-mijozlar');
+  const slidesFile6 = document.querySelector('.carusel-inner-mijozlar');
   const line1 = document.getElementById('line1');
   const line4 = document.getElementById('line4');
   const line7 = document.getElementById('line7');
@@ -37,40 +49,27 @@ window.addEventListener('DOMContentLoaded', () => {
   const line13 = document.getElementById('line13');
   const line16 = document.getElementById('line16');
 
-  slidesFile.style.width = slides.length * 100 + '%';
-  slidesFile_2.style.width = slides_2.length * 100 + '%';
-  slidesFile_3.style.width = slides_3.length * 100 + '%';
-  slidesFile_4.style.width = slides_4.length * 100 + '%';
-  slidesFile_5.style.width = slides_5.length * 100 + '%';
-  slidesFile_6.style.width = slides_6.length * 100 + '%';
+  slidesFile.style.width = `${slides.length * 100}%`;
+  slidesFile2.style.width = `${slides2.length * 100}%`;
+  slidesFile3.style.width = `${slides3.length * 100}%`;
+  slidesFile4.style.width = `${slides4.length * 100}%`;
+  slidesFile5.style.width = `${slides5.length * 100}%`;
+  slidesFile6.style.width = `${slides6.length * 100}%`;
   setActiveButton(line1, '.lines');
   setActiveButton(line4, '.lines_2');
   setActiveButton(line7, '.lines_3');
   setActiveButton(line10, '.lines_4');
   setActiveButton(line13, '.lines_5');
   setActiveButton(line16, '.lines_6');
-})
-
-const setActiveButton = (button, str) => {
-  const buttons = document.querySelectorAll(str);
-
-  buttons.forEach((button) => {
-    if (button !== this) {
-      button.classList.remove('active');
-    }
-  });
-  button.classList.add('active');
-};
+});
 
 const slider = document.querySelector('.carusel-inner');
-const slider_2 = document.querySelector('.carusel-inner-kurslar');
-const slider_3 = document.querySelector('.carusel-inner-ustozlar');
-const slider_4 = document.querySelector('.carusel-inner-videolar');
-const slider_5 = document.querySelector('.carusel-inner-oquvchilar');
-const slider_6 = document.querySelector('.carusel-inner-mijozlar');
-// const line1 = document.getElementById('line1');
-// const line2 = document.getElementById('line2');
-// const line3 = document.getElementById('line3');
+const slider2 = document.querySelector('.carusel-inner-kurslar');
+const slider3 = document.querySelector('.carusel-inner-ustozlar');
+const slider4 = document.querySelector('.carusel-inner-videolar');
+const slider5 = document.querySelector('.carusel-inner-oquvchilar');
+const slider6 = document.querySelector('.carusel-inner-mijozlar');
+
 document.addEventListener('click', (e) => {
   if (e.target.classList.contains('line1')) {
     if (e.target.classList.contains('active')) return;
@@ -87,63 +86,63 @@ document.addEventListener('click', (e) => {
   } else if (e.target.classList.contains('line4')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_2');
-    slider_2.style.transform = 'translateX(0)';
+    slider2.style.transform = 'translateX(0)';
   } else if (e.target.classList.contains('line5')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_2');
-    slider_2.style.transform = 'translateX(-33.33%)';
+    slider2.style.transform = 'translateX(-33.33%)';
   } else if (e.target.classList.contains('line6')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_2');
-    slider_2.style.transform = 'translateX(-66.66%)';
+    slider2.style.transform = 'translateX(-66.66%)';
   } else if (e.target.classList.contains('line7')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_3');
-    slider_3.style.transform = 'translateX(0)';
+    slider3.style.transform = 'translateX(0)';
   } else if (e.target.classList.contains('line8')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_3');
-    slider_3.style.transform = 'translateX(-33.33%)';
+    slider3.style.transform = 'translateX(-33.33%)';
   } else if (e.target.classList.contains('line9')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_3');
-    slider_3.style.transform = 'translateX(-66.66%)';
+    slider3.style.transform = 'translateX(-66.66%)';
   } else if (e.target.classList.contains('line10')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_4');
-    slider_4.style.transform = 'translateX(0)';
+    slider4.style.transform = 'translateX(0)';
   } else if (e.target.classList.contains('line11')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_4');
-    slider_4.style.transform = 'translateX(-33.33%)';
+    slider4.style.transform = 'translateX(-33.33%)';
   } else if (e.target.classList.contains('line12')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_4');
-    slider_4.style.transform = 'translateX(-66.66%)';
+    slider4.style.transform = 'translateX(-66.66%)';
   } else if (e.target.classList.contains('line13')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_5');
-    slider_5.style.transform = 'translateX(0)';
+    slider5.style.transform = 'translateX(0)';
   } else if (e.target.classList.contains('line14')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_5');
-    slider_5.style.transform = 'translateX(-33.33%)';
+    slider5.style.transform = 'translateX(-33.33%)';
   } else if (e.target.classList.contains('line15')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_5');
-    slider_5.style.transform = 'translateX(-66.66%)';
+    slider5.style.transform = 'translateX(-66.66%)';
   } else if (e.target.classList.contains('line16')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_6');
-    slider_6.style.transform = 'translateX(0)';
+    slider6.style.transform = 'translateX(0)';
   } else if (e.target.classList.contains('line17')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_6');
-    slider_6.style.transform = 'translateX(-33.33%)';
+    slider6.style.transform = 'translateX(-33.33%)';
   } else if (e.target.classList.contains('line18')) {
     if (e.target.classList.contains('active')) return;
     setActiveButton(e.target, '.lines_6');
-    slider_6.style.transform = 'translateX(-66.66%)';
+    slider6.style.transform = 'translateX(-66.66%)';
   } else if (e.target.classList.contains('ul-li')) {
     menu.innerHTML = `<div class="close-bar"></div>
     <div class="close-bar"></div>
@@ -152,5 +151,4 @@ document.addEventListener('click', (e) => {
     menu.classList.toggle('open');
     navbar.classList.toggle('d-none');
   }
-})
-
+});
